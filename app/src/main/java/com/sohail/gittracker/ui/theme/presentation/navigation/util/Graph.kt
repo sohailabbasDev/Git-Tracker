69bf277e940ed40837a401078e36c4a1ed6928df
Sohail abbas <108873318+sohailabbasDev@users.noreply.github.com>
2023-09-25 19:42:20 +0530

    Delete app/src/main/java/com/sohail/gittracker/ui/theme/presentation directory

--- app/src/main/java/com/sohail/gittracker/ui/theme/presentation/navigation/util/Graph.kt
+++
@@ -1,5 +0,0 @@
-package com.sohail.gittracker.ui.theme.presentation.navigation.util
-
-object Graph {
-    const val MAIN = "main_graph"
-}
--- app/src/main/java/com/sohail/gittracker/ui/theme/presentation/navigation/util/MainRoutes.kt
+++
@@ -1,6 +0,0 @@
-package com.sohail.gittracker.ui.theme.presentation.navigation.util
-
-sealed class MainRoutes(val route : String){
-    object Splash : MainRoutes("SPLASH_SCREEN")
-    object Home : MainRoutes("HOME-SCREEN")
-}
