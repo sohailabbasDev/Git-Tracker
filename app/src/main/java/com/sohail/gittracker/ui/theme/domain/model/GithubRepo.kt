e757b71b79db3675d075f1f24c3cdff0e1ef3f0f
Sohail abbas <108873318+sohailabbasDev@users.noreply.github.com>
2023-09-25 19:42:07 +0530

    Delete app/src/main/java/com/sohail/gittracker/ui/theme/domain directory

--- app/src/main/java/com/sohail/gittracker/ui/theme/domain/model/GithubRepo.kt
+++
@@ -1,7 +0,0 @@
-package com.sohail.gittracker.ui.theme.domain.model
-
-data class GithubRepo(
-    val avatarUrl : String,
-    val name : String,
-    val description : String,
-)
--- app/src/main/java/com/sohail/gittracker/ui/theme/domain/use_case/GitRepoUseCases.kt
+++
@@ -1,8 +0,0 @@
-package com.sohail.gittracker.ui.theme.domain.use_case
-
-data class GitRepoUseCases(
-    val getGitRepoUseCase: GetGitRepoUseCase,
-    val addRepoToDbUseCase: AddRepoToDbUseCase,
-    val getAllReposFromDbUseCase: GetAllReposFromDbUseCase,
-    val deleteRepoUseCase: DeleteRepoUseCase
-)
